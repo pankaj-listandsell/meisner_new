eca0d53f3064fc95af821f25ecb79feef43d3338
Pankaj <pankajlistandsell@gmail.com>
2026-06-18 18:30:56 +0530

    add forward slash (/) in the end of all pages in sitemap

--- modules/Core/Views/frontend/sitemap-path.blade.php
+++ modules/Core/Views/frontend/sitemap-path.blade.php
@@ -1,7 +1,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach($rows as $url)
         <url>
-            <loc>{{$url['loc']}}</loc>
+            <loc>{{rtrim($url['loc'], '/') . '/'}}</loc>
             <lastmod>{{$url['lastmod'] ?? ''}}</lastmod>
         </url>
     @endforeach
