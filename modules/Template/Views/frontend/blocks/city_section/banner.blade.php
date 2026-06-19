8f6431d7dbf044ddf9ed12b228c6db8fec60de6c
Pankaj <pankajlistandsell@gmail.com>
2026-06-19 16:02:05 +0530

    add forward slash (/) in the end of city and servise page banner  page url

--- modules/Template/Views/frontend/blocks/city_section/banner.blade.php
+++ modules/Template/Views/frontend/blocks/city_section/banner.blade.php
@@ -7,7 +7,7 @@
                <h1>{{ $title }}</h1>
                {!! $content !!}
             <div class="row">
-                <div class="col-lg-4 col-md-5 col-sm-12"><a href="{{ $button_link }}" class="banner-btn" >{{ $button_text }}</a></div>
+                <div class="col-lg-4 col-md-5 col-sm-12"><a href="{{ rtrim($button_link, '/') . '/' }}" class="banner-btn" >{{ $button_text }}</a></div>
                 <div class="col-lg-6 col-md-7 col-sm-12">
                   <div class="banner-cta">
                       <a href="tel:{{ setting_item("phone_no_link") }}"><img title="{{ setting_item("phone_no") }}" alt="{{ setting_item("phone_no") }}" src="/assests/img/icons/green-telephone.svg"> {{ setting_item("phone_no") }}</a>
